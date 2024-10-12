c5b13c791b6a90c76a6cd40b1831937437055836
Nithish kumar K <knithish432@gmail.com>
2024-10-13 01:43:57 +0530

    some changes

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
-// export const API = "https://668ea164bf9912d4c92f2326.mockapi.io";
+export const API = "https://668ea164bf9912d4c92f2326.mockapi.io";
 // export const API = "http://localhost:4000";
-export const API = "https://mongo-node-production.up.railway.app";
+// export const API = "https://mongo-node-production.up.railway.app";
