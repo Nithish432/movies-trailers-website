cee2a48a9707a71b171df9371fe66b48ba63814d
Nithish kumar K <knithish432@gmail.com>
2024-10-14 16:20:47 +0530

    some updates

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
-// export const API = "https://668ea164bf9912d4c92f2326.mockapi.io";
+export const API = "https://668ea164bf9912d4c92f2326.mockapi.io";
 // export const API = "http://localhost:4000";
-export const API = "https://mongo-node-uymu.onrender.com";
+// export const API = "https://mongo-node-uymu.onrender.com";
